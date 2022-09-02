6d32cad0dd63bd8f242af513aed7da81973ebf3a
amendoxe <arturo_mendoza@outlook.com>
2022-09-01 23:11:12 -0500

    Add functioning code, except for the left arrow

--- script.js
+++ script.js
@@ -1,35 +1,106 @@
 const screen = document.querySelector(".screen-output").value;
 let receiver = screen;
-console.log(`the receiver ${receiver}`);
-console.log(screen);
+let number = parseInt(receiver);
+let memory;
+let result;
+function addToReceiver(event) {
+	receiver += event.target.innerText;
+	document.querySelector(".screen-output").value = receiver;
+}
+function storeIt() {
+	memory = parseInt(receiver);
+	console.log(`memory is: ${memory}`);
+}
+function eraseIt() {
+	document.querySelector(".screen-output").value = "";
+	receiver = "";
+}
+function operation(num) {
+	if (num == 1) {
+		result = memory / parseInt(receiver);
+	}
+	if (num == 2) {
+		result = memory * parseInt(receiver);
+	}
+	if (num == 3) {
+		result = memory - parseInt(receiver);
+	}
+	if (num == 4) {
+		result = memory + parseInt(receiver);
+	}
+
+	console.log(`operation is: ${num}`);
+}
 document.querySelector(".button-container").addEventListener("click", function (event) {
 	console.log(event.target.innerText);
-	let out = "";
 	if (event.target.innerText === "C") {
-		document.querySelector(".screen-output").value = 0;
+		eraseIt();
 	}
-	if (event.target.innerText == "&#x2190;") {
-		document.querySelector(".screen-output").value = document.querySelector(".screen-output").value.slice(0, -1);
+	if (event.target.id == "left-arrow") {
+		receiver.slice(0, -1);
+		document.querySelector(".screen-output").value = receiver;
 		console.log(receiver);
 	}
 	if (event.target.innerText == 0) {
+		addToReceiver(event);
 	}
-	if (event.target.innerText == 0) {
+	if (event.target.innerText == 1) {
+		addToReceiver(event);
 	}
-	if (event.target.innerText == 0) {
+	if (event.target.innerText == 2) {
+		addToReceiver(event);
 	}
-	if (event.target.innerText == 0) {
+	if (event.target.innerText == 3) {
+		addToReceiver(event);
 	}
-	if (event.target.innerText == 0) {
+	if (event.target.innerText == 4) {
+		addToReceiver(event);
 	}
-	if (event.target.innerText == 0) {
+	if (event.target.innerText == 5) {
+		addToReceiver(event);
 	}
-	if (event.target.innerText == 0) {
+	if (event.target.innerText == 6) {
+		addToReceiver(event);
 	}
-	if (event.target.innerText == 0) {
+	if (event.target.innerText == 7) {
+		addToReceiver(event);
 	}
-	if (event.target.innerText == 0) {
+	if (event.target.innerText == 8) {
+		addToReceiver(event);
 	}
-	if (event.target.innerText == 0) {
+	if (event.target.innerText == 9) {
+		addToReceiver(event);
+	}
+	if (event.target.id == "divide") {
+		//guarda número hasta ahora y escribe otro número
+		storeIt();
+		eraseIt();
+		num = 1;
+	}
+	if (event.target.id == "times") {
+		storeIt();
+		eraseIt();
+		num = 2;
+	}
+	if (event.target.id == "minus") {
+		storeIt();
+		eraseIt();
+		num = 3;
+	}
+	if (event.target.id == "plus") {
+		storeIt();
+		eraseIt();
+		num = 4;
+	}
+	if (event.target.id == "equals") {
+		operation(num);
+		console.log(`Resultado de la operación: ${result}`);
+		receiver = result;
+		document.querySelector(".screen-output").value = receiver;
+		console.log(document.querySelector(".screen-output").value);
+		num = 0;
+		storeIt();
+		// eraseIt();
+		// console.log(`receiver equals to :${receiver}`);
 	}
 });
